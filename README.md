19d55d4e54f440bd8de60ccaf6c89e95ec465465
curriejames <curriejames@icloud.com>
2021-11-12 16:27:20 -0500

    saving files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # NYC_Guide
+
+
+### Group Members<br>
+**James Currie**<br>Github Username `curriejames`   
+**Oma Strachan**<br>Github Username
+`stradroma`
+<br>
+<br>
+*CREATE & RUN VIRTUAL ENVIRONMENT*<br>
+- from your django-projects folder
+run `python3 -m venv django-env` to create a new virtual environment<br>
+- then run `source django-env/bin/activate` on Unix/MacOS or `django-env\Scripts\activate.bat` on Windows to activate the virtual environment<br>
+- you can run `django-env\Scripts\activate.ps1` too if `.bat`  doesn't work
+<br>
+<br>
+
+*INSTALL PROJECT DEPENDENCIES WITH PIP*<br>
+- run `pip install insertpackagehere` to install<br>
+- run `pip freeze > requirements.txt` to install the dependencies of the package
+<br>
+<br>
+
+*RUN DJANGO APPLICATION*<br>
+- use need the `manage.py` file to run the application aka server.
+- run `python manage.py runserver`
+- Go to local host `http://localhost:8000`
+
+
---
+++ nyc-guide-main/nyc/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .boroughs import boroughs
+
+def city(request):
+    if request.method == 'GET':
+        return render(request=request, template_name='city.html', context={ 'boroughs': boroughs.keys() })
+
+def borough(request, borough):
+    if request.method == 'GET':
+        return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
+
+# Create your views here.
+def activity(request, borough,activity):
+    if request.method == 'GET':
+        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'activities': boroughs[borough][activity].keys()})
+
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={'borough': borough,'activity':
+        activity, 'venue': venue, 'description': boroughs[borough][activity][venue].get('description')})
